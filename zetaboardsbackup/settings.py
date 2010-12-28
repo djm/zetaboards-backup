c4e250f7064bfbbc90bd5ab5ec8d89328f086723
Darian Moody <mail@djm.org.uk>
2010-12-28 15:47:27 +0000

    Sorted fuck up.

---
+++ zetaboardsbackup/settings.py
@@ -0,0 +1,75 @@
+import os
+PROJECT_ROOT = os.path.dirname(os.path.realpath(__file__))
+here = lambda *x: os.path.join(PROJECT_ROOT, *x)
+
+DEBUG = False
+TEMPLATE_DEBUG = DEBUG
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'zetaboardsbackup',
+        'USER': 'zetaboardsbackup',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    }
+}
+
+TIME_ZONE = 'Europe/London'
+
+LANGUAGE_CODE = 'en-gb'
+
+SITE_ID = 1
+
+USE_I18N = True
+
+USE_L10N = True
+
+# URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
+# trailing slash.
+# Examples: "http://foo.com/media/", "/media/".
+ADMIN_MEDIA_PREFIX = '/media/'
+
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = 'tp8sv(yii4==r64w7z^k+cy9p^$+!!@6k@n8nnnaoaa!24z=hn'
+
+# List of callables that know how to import templates from various sources.
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+)
+
+ROOT_URLCONF = 'zetaboardsbackup.urls'
+
+TEMPLATE_DIRS = (
+    here('templates'),
+)
+
+INSTALLED_APPS = (
+    # Django Apps
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.messages',
+    'django.contrib.admindocs',
+    'django.contrib.admin',
+    # 3rd Party Apps
+    'south',
+    # Project Apps
+    'forum',
+)
+
+try:
+    from settings_local import *
+except ImportError:
+    pass
