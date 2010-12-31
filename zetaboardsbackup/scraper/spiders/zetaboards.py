59ec7dfa52dceec0c001d381b6418672a9858cf6
Darian Moody <mail@djm.org.uk>
2010-12-31 05:09:24 +0000

    Members saving, whoooop.

--- zetaboardsbackup/scraper/spiders/zetaboards.py
+++ zetaboardsbackup/scraper/spiders/zetaboards.py
@@ -115,7 +115,6 @@ class ZetaboardsSpider(BaseSpider):
         """
         Crawl the main index to get all root categories.
         """
-        import ipdb; ipdb.set_trace();
         hxs = HtmlXPathSelector(response)
         # Get all root categories on the page, store them and then fire of a 
         # request to go visit their specific subpage so we can get the subcats.
